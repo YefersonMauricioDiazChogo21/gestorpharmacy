b9f2fe72174f3b1eddd61711f4e70d2ed717091f
YefersonMauricioiDiazChogo21 <yefersondiaz2001@gmail.com>
2024-08-15 11:59:23 -0500

    docs

--- src/main/java/com/pharmacy/country/infraestructure/CountryRepository.java
+++ src/main/java/com/pharmacy/country/infraestructure/CountryRepository.java
@@ -1,23 +1,42 @@
 package com.pharmacy.country.infraestructure;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
-import  com.pharmacy.country.domain.entity.Country;
+import  com.pharmacy.Database;
+import com.pharmacy.country.domain.entity.Country;
 import com.pharmacy.country.domain.service.CountryService;
 
 public class CountryRepository implements CountryService{
+    private Connection connection;
 
     @Override
     public void createCountry(Country country) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createCountry'");
+        try (Connection connection = Database.getConnection()){
+            String query = "INSERT INTO country (codecountry,namecountry) VALUES (?,?)";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, country.getCodecountry());
+            ps.setString(2, country.getNamecountry());
+            ps.executeUpdate();            
+        } catch (SQLException e) {
+            // TODO: handle exception
+        }
     }
 
     @Override
     public void updateCountry(Country country) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateCountry'");
+        try (Connection connection = Database.getConnection()){
+            String query = "UPDATE country SET codecountry= ?,namecountry= ?";
+            PreparedStatement ps = connection.prepareStatement(query);
+            ps.setString(1, country.getCodecountry());
+            ps.setString(2, country.getNamecountry());
+            ps.executeUpdate();            
+        } catch (SQLException e) {
+            // TODO: handle exception
+        }
     }
 
     @Override
@@ -26,10 +45,11 @@ public class CountryRepository implements CountryService{
         throw new UnsupportedOperationException("Unimplemented method 'deleteCountry'");
     }
 
+   
     @Override
     public Optional<Country> findCountryById(String codecountry) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findCountryById'");
+       // TODO Auto-generated method stub
+   throw new UnsupportedOperationException("Unimplemented method 'findAllCountries'");
     }
 
     @Override
